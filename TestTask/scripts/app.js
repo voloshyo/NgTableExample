4ec22232209c4d86f20fdc7a1e5e957e53dbbebc
unknown <ovoloshyna@DC10_LUXOFT13.kiev.luxoft.com>
2014-11-17 11:48:05 +0200

    add src files

---
+++ TestTask/scripts/app.js
@@ -0,0 +1,32 @@
+﻿var mainmodule = angular.module('main', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ngTable']);
+
+mainmodule.factory('getData', function ($http) {
+
+    return {
+        shortData:  $http.get('../data/products.txt'),
+        largeData: $http.get('../data/productsLarge.txt')
+       };
+});
+
+mainmodule.controller('mainCtrl', function ($rootScope, $q, getData) {
+
+    var ppromises = [getData.shortData, getData.largeData];
+
+    $q.all(ppromises).then(function (result) {
+        var tmp = [];
+        angular.forEach(result, function (responce) {
+            tmp.push(responce.data);
+        });
+        return tmp;
+    }).then(function (tempRes) {
+        var t = [];
+        _(tempRes).each(function (item) { t.push(item); });
+        return t;
+    }).then(function (tmpr) {
+        $rootScope.shlist = tmpr[0];
+        $rootScope.llist = tmpr[1];
+    });
+});
+
+
+
---
+++ TestTask/scripts/storetable.js
@@ -0,0 +1,52 @@
+﻿mainmodule
+    .directive('storetable', function () {
+        return {
+            restrict: "E",
+            templateUrl: '../views/storetable.html'
+        };
+    })
+
+.controller('storetableCtrl', function ($scope, $rootScope, ngTableParams, $filter) {
+
+    function init() {
+        var rs = $rootScope;
+        $scope.mode = 'short';
+        $scope.products = rs.shlist;
+        $scope.currentPage = 1;
+        $scope.productsToView = function() { return $scope.products.slice(0, 50); };
+    };
+    init();
+    
+
+    $scope.$watch('mode', function() {
+        if ($scope.mode === 'short') {
+            $scope.products = $rootScope.shlist;
+        };
+        
+        if ($scope.mode === 'large') {
+            $scope.products =  $rootScope.llist;
+        };
+            $scope.productsToView = function() { return $scope.products.slice((($scope.currentPage - 1) * 50), ($scope.currentPage) * 50); };
+    });
+
+
+    //var data = $scope.productsToView();
+
+    //$scope.tableParams = new ngTableParams({
+    //    page: 1,            // show first page
+    //    count: 10,          // count per page
+    //    sorting: {
+    //        name: 'asc'     // initial sorting
+    //    }
+    //}, {
+    //    total: data.length, // length of data
+    //    getData: function ($defer, params) {
+    //        // use build-in angular filter
+    //        var orderedData = params.sorting() ?
+    //                            $filter('orderBy')(data, params.orderBy()) :
+    //                            data;
+
+    //        $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+    //    }
+    //});
+})
